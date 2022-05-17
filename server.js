75fa14da8825ce78dacfbe09c34263e762392cee
Kuldeeprajput <kuldeep.rajput@300minds.com>
2022-05-17 17:48:34 +0530

    server file - changed

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 const routes = require('./src/routes');
 
-app.use('/', routes);
 
 
-app.listen(3000,()=>{
-  console.log("Server running on port 3000")
-})
+
+app.use("/", routes);
+
+app.listen(3000, () => {
+  console.log("temppppp");
+  console.log("Server running on port 3000");
+});
