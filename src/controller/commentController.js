c47131fe09db0193c0a5497ce70e40697d853fe9
Kuldeeprajput <kuldeep.rajput@300minds.com>
2022-05-18 11:42:09 +0530

    add async module demo.

--- src/controller/commentController.js
+++ src/controller/commentController.js
@@ -1,6 +1,6 @@
 const {User, Comment, Post} = require('./../models/index')
 const Mongoose = require('mongoose')
-
+const async = require("async");
 module.exports = {
   createComment: async (req, res) => {
     const authID = req.body.user_id;
@@ -152,5 +152,36 @@ module.exports = {
         console.log(temp, "temp");
         res.send(temp);
       });
+  },
+
+  asyncDemo: (req, res) => {
+    var q = async.queue(function (task, callback) {
+      console.log("Hello " + task.name);
+      callback();
+    }, 2);
+
+    // assign a callback
+    q.drain = function () {
+      console.log("All items have been processed");
+    };
+
+    // add some items to the queue
+    q.push({ name: "foo" }, function (err) {
+      console.log("Finished processing foo");
+    });
+
+    q.push({ name: "bar" }, function (err) {
+      console.log("Finished processing bar");
+    });
+
+    // add some items to the queue (batch-wise)
+    q.push([{ name: "baz" }, { name: "bay" }, { name: "bax" }], function (err) {
+      console.log("Finished processing item");
+    });
+
+    // add some items to the front of the queue
+    q.unshift({ name: "bar" }, function (err) {
+      console.log("Finished processing bar");
+    });
   },
 };
